ce0e3158fef279193dcf8b6feb4d0d9270e93c16
yargrinders <yargrindersp20@gmail.com>
2024-11-12 14:57:25 +0100

    v1_12_11

---
+++ README.md
@@ -0,0 +1,19 @@
+# Vue Cli für Website-Layouts
+
+Dokumentation und Anwendungsbeispiele sind enthalten.
+
+Abhängigkeiten installieren:
+```
+npm i
+```
+
+Im Entwicklungsmodus starten:
+```
+npm run serve 
+```
+
+Version für Veröffentlichung erstellen:
+```
+npm run build
+```
+
---
+++ src/HomeView.vue
@@ -0,0 +1,47 @@
+<template>
+
+	<!-- Main section start -->
+	<main-section></main-section>
+	<!-- Main section end -->
+
+	<!-- About section start -->
+	<about-section></about-section>
+	<!-- About section end -->
+
+	<!-- Services section start -->
+	<service-section></service-section>
+	<!-- Services section end -->
+
+	<!-- Portfolio section start -->
+	<portfolio-section></portfolio-section>
+	<!-- Portfolio section end -->
+
+
+	<!-- Contact section start -->
+	<contact-section></contact-section>
+	<!-- Contact section end -->
+
+</template>
+
+<script>
+import MainSection from '@/sections/MainSection.vue';
+import AboutSection from '@/sections/AboutSection.vue';
+import ServiceSection from '@/sections/ServiceSection.vue';
+import PortfolioSection from '@/sections/PortfolioSection.vue';
+import ContactSection from '@/sections/ContactSection.vue';
+
+export default {
+	components: {
+		MainSection,
+		AboutSection,
+		ServiceSection,
+		PortfolioSection,
+		ContactSection,
+	}
+}
+</script>
+
+<style scoped>
+
+
+</style>
